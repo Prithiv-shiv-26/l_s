a8863a5bc3aaa82218cbbc07c5bd194ef2e798cc
Prithiv-shiv-26 <prithivshiv26@gmail.com>
2025-12-28 23:26:14 +0530

    cred encrypt

--- server/src/data-source.ts
+++ server/src/data-source.ts
@@ -7,11 +7,12 @@ import "dotenv/config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "password",
-  database: "library_db",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
-  entities: [Book, User, IssueRecord],
+  logging: false,
+  entities: [User, Book, IssueRecord],
 });
